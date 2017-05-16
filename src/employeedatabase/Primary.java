7c50fb095357b2d44d47d9ce2846dc582ece5587
ahan <alvinhan1999@gmail.com>
2017-05-16 16:04:43 -0400

    included primary.java

---
+++ src/employeedatabase/Primary.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeedatabase;
+
+/**
+ *
+ * @author Alvin
+ */
+
+public class Primary {
+    
+    static EmployeeTable bigList = new EmployeeTable(10);
+
+    public static void main(String args[]) {
+        GUI thing = new GUI();
+    }
+    
+}
